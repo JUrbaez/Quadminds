6637e6ba987f64ce8c22f77e293945370309f984
Jesus Urbaez <jurbaez@secureauth.com>
2019-11-29 14:43:10 -0300

    Final Code

---
+++ ReadMe.txt
@@ -0,0 +1,14 @@
+Quadminds - Examen de Automatizacion
+
+1) Pre-requisitos
+
+Java JDK 1.8 minimo
+Maven
+Eclipse Java (O cualquier IDE con setup de JDK) 
+
+2) Ejecucion de las pruebas 
+
+	a) Realizar el setup del IDE con Java + Maven
+	b) Click derecho en el archivo "TestNG.xml" -> RunAs "TestNG Suite"
+
+
--- src/main/java/FrontEndTests/FE_Tests.java
+++ src/main/java/FrontEndTests/FE_Tests.java
@@ -49,6 +49,7 @@ public class FE_Tests {
 	@AfterTest
 	private static void closeDriver() 
 	{
+		System.out.println("--------------------Test Finished ---------------");
 		driver.quit();
 	}
 	
@@ -65,8 +66,9 @@ public class FE_Tests {
 	}
 	
 	@Test(description = "Verificar categoria - Climatizacion")
-    public static void verifyCategoryClimatizacion() 
+    public static void verifySubCategoryClimatizacion() 
     {
+	   System.out.println("--------------------Starting <Verificar categoria - Climatizacion> ---------------");
        try
        {
            driver.get(Url);
@@ -95,9 +97,138 @@ public class FE_Tests {
        }
     }
 	
+	@Test(description = "Verificar categoria - Tecnología - Celulares y Smartphones")
+    public static void verifySubCategoryCS() 
+    {
+	   System.out.println("--------------------Starting <Verificar categoria - Tecnología - Celulares y Smartphones> ---------------");
+       try
+       {
+           driver.get(Url);
+           mlPage = new MainPage(driver);
+           WebElement catMenu = mlPage.getCatMenu();
+           mouseAction(driver, catMenu);
+       	   
+       	   newExplicitWait(driver, "//a[contains(text(),'Tecnología')]");
+       	       	   
+       	   WebElement subCat = driver.findElement(By.xpath("//a[contains(text(),'Tecnología')]"));	
+       	   mouseAction(driver, subCat);
+           driver.findElement(By.xpath("//a[contains(text(),'Celulares y Smartphones')]")).click();
+           
+           String catTitle = driver.findElement(By.xpath("//h1[contains(@class,'breadcrumb__title')]")).getText();
+           String catNumber = driver.findElement(By.xpath("//div[@class='quantity-results']")).getText();
+           String [] catNumber2 = catNumber.split(" ", 0);
+         
+           final SoftAssert softAssert = new SoftAssert();
+		   softAssert.assertTrue(catTitle.equalsIgnoreCase("Celulares y Smartphones"));
+		   softAssert.assertTrue(Double.parseDouble(catNumber2[0])>0);
+		   softAssert.assertAll();
+       }
+       catch(Exception e) 
+       {
+           System.out.println(e);
+       }
+    }
+	
+	@Test(description = "Verificar categoria - Tecnología - Drones")
+    public static void verifySubCategoryDrones() 
+    {
+	   System.out.println("--------------------Starting <Verificar categoria - Tecnología - Drones> ---------------");
+       try
+       {
+           driver.get(Url);
+           mlPage = new MainPage(driver);
+           WebElement catMenu = mlPage.getCatMenu();
+           mouseAction(driver, catMenu);
+       	   
+       	   newExplicitWait(driver, "//a[contains(text(),'Tecnología')]");
+       	       	   
+       	   WebElement subCat = driver.findElement(By.xpath("//a[contains(text(),'Tecnología')]"));	
+       	   mouseAction(driver, subCat);
+           driver.findElement(By.xpath("//a[contains(text(),'Drones y Accesorios')]")).click();
+           
+           String catTitle = driver.findElement(By.xpath("//h1[contains(@class,'breadcrumb__title')]")).getText();
+           String catNumber = driver.findElement(By.xpath("//div[@class='quantity-results']")).getText();
+           String [] catNumber2 = catNumber.split(" ", 0);
+         
+           final SoftAssert softAssert = new SoftAssert();
+		   softAssert.assertTrue(catTitle.equalsIgnoreCase("Drones y Accesorios"));
+		   softAssert.assertTrue(Double.parseDouble(catNumber2[0])>0);
+		   softAssert.assertAll();
+       }
+       catch(Exception e) 
+       {
+           System.out.println(e);
+       }
+    }
+	
+	@Test(description = "Verificar categoria - Herramientas e Industrias - Textiles")
+    public static void verifySubCategoryTextiles() 
+    {
+	   System.out.println("--------------------Starting <Verificar categoria - Herramientas e Industrias - Textiles> ---------------");
+       try
+       {
+           driver.get(Url);
+           mlPage = new MainPage(driver);
+           WebElement catMenu = mlPage.getCatMenu();
+           mouseAction(driver, catMenu);
+       	   
+       	   newExplicitWait(driver, "//a[contains(text(),'Herramientas e Industrias')]");
+       	       	   
+       	   WebElement subCat = driver.findElement(By.xpath("//a[contains(text(),'Herramientas e Industrias')]"));	
+       	   mouseAction(driver, subCat);
+           driver.findElement(By.xpath("//a[contains(text(),'Industria Textil')]")).click();
+           
+           String catTitle = driver.findElement(By.xpath("//h1[contains(@class,'breadcrumb__title')]")).getText();
+           String catNumber = driver.findElement(By.xpath("//div[@class='quantity-results']")).getText();
+           String [] catNumber2 = catNumber.split(" ", 0);
+         
+           final SoftAssert softAssert = new SoftAssert();
+		   softAssert.assertTrue(catTitle.equalsIgnoreCase("Industria Textil"));
+		   softAssert.assertTrue(Double.parseDouble(catNumber2[0])>0);
+		   softAssert.assertAll();
+       }
+       catch(Exception e) 
+       {
+           System.out.println(e);
+       }
+    }
+	
+	@Test(description = "Verificar categoria - Juguetes y Bebes - Cuarto")
+    public static void verifySubCategoryBebes() 
+    {
+	   System.out.println("--------------------Starting <Verificar categoria - Juguetes y Bebes - Cuarto> ---------------");
+       try
+       {
+           driver.get(Url);
+           mlPage = new MainPage(driver);
+           WebElement catMenu = mlPage.getCatMenu();
+           mouseAction(driver, catMenu);
+       	   
+       	   newExplicitWait(driver, "//a[contains(text(),'Juguetes y Bebés')]");
+       	       	   
+       	   WebElement subCat = driver.findElement(By.xpath("//a[contains(text(),'Juguetes y Bebés')]"));	
+       	   mouseAction(driver, subCat);
+           driver.findElement(By.xpath("//a[contains(text(),'Cuarto del Bebé')]")).click();
+           
+           String catTitle = driver.findElement(By.xpath("//h1[contains(@class,'breadcrumb__title')]")).getText();
+           String catNumber = driver.findElement(By.xpath("//div[@class='quantity-results']")).getText();
+           String [] catNumber2 = catNumber.split(" ", 0);
+         
+           final SoftAssert softAssert = new SoftAssert();
+		   softAssert.assertTrue(catTitle.equalsIgnoreCase("Cuarto del Bebé"));
+		   softAssert.assertTrue(Double.parseDouble(catNumber2[0])>0);
+		   softAssert.assertAll();
+       }
+       catch(Exception e) 
+       {
+           System.out.println(e);
+       }
+    }
+	
 	@Test(description = "Verificar la grilla de Celulares y Smartphones")
     public static void verifySmartCellsGrid() 
     {
+	   System.out.println("--------------------Starting <Verificar la grilla de Celulares y Smartphones> ---------------");
        try
        {
            driver.get(Url);
@@ -113,7 +244,7 @@ public class FE_Tests {
            driver.findElement(By.xpath("//a[contains(text(),'Celulares y Smartphones')]")).click();
            driver.findElement(By.xpath("//div[@class='ico view-option-grid']")).click();
            
-           System.out.println(driver.findElements(By.xpath("//ol[@id='searchResults']//form")).size());
+           System.out.println("El numero de Items en la grilla es: " + driver.findElements(By.xpath("//ol[@id='searchResults']//form")).size());
        }
        catch(Exception e) 
        {
@@ -124,6 +255,7 @@ public class FE_Tests {
 	@Test(description = "Verificar el mayor y el menor precio de una categoria")
     public static void verifyPrices() 
     {
+	   System.out.println("--------------------Starting <Verificar el mayor y el menor precio de una categoria> ---------------");
        try
        {
     	   driver.get(Url);
@@ -163,9 +295,47 @@ public class FE_Tests {
        }
     }
 	
+	@Test(description = "Verificar Envio Gratis")
+    public static void verifyShipment() 
+    {
+	   System.out.println("--------------------Starting <Verificar Envio Gratis> ---------------");
+       try
+       {
+           driver.get(Url);
+           mlPage = new MainPage(driver);
+           WebElement catMenu = mlPage.getCatMenu();
+           mouseAction(driver, catMenu);
+       	   
+           newExplicitWait(driver, "//a[contains(text(),'Tecnología')]");
+       	   
+       	   WebElement subCat = driver.findElement(By.xpath("//a[contains(text(),'Tecnología')]"));	
+       	   
+       	   mouseAction(driver, subCat);
+    	   
+           driver.findElement(By.xpath("//a[contains(text(),'Drones y Accesorios')]")).click();
+           
+           List<WebElement> itemList = driver.findElements(By.xpath("//div[@class='item__info-container highlighted ']//p"));
+           
+           final SoftAssert softAssert = new SoftAssert();
+           
+           for(int i=0; i<5; i++) 
+           {
+    		   softAssert.assertTrue(itemList.get(i).getText().equalsIgnoreCase("Llegan hoy") || itemList.get(i).getText().equalsIgnoreCase("Llega Gratis Hoy"));
+           }
+        
+           
+		   softAssert.assertAll();
+       }
+       catch(Exception e) 
+       {
+           System.out.println(e);
+       }
+    }
+	
 	@Test(description = "Verify Free Shipping Bar")
     public static void ejercicio2() 
     {
+	   System.out.println("--------------------Starting Ejercicio 2---------------");
        try
        {
            driver.get(Url);
@@ -182,7 +352,7 @@ public class FE_Tests {
            driver.findElement(By.xpath("//a[contains(text(),'Celulares y Smartphones')]")).click();
            
            List<WebElement> itemList = driver.findElements(By.xpath("//div[@class='item__info item--show-right-col ']"));
-           WebElement el1 = itemList.get(0).findElement(By.xpath("//div[@class='item__info item--show-right-col ']//h2"));
+           WebElement el1 = itemList.get(0).findElement(By.xpath("//div[@class='item__info item--show-right-col ']//h2//span"));
            System.out.println(el1.getText());
            
            WebElement el2 = itemList.get(0).findElement(By.xpath("//div[@class='item__info item--show-right-col ']//span[@class='price__fraction']"));
@@ -193,18 +363,22 @@ public class FE_Tests {
            
            el1.click();
            
+           newExplicitWait(driver, "//h1[@class='ui-pdp-title']");
+           
            WebElement el3 = driver.findElement(By.xpath("//h1[@class='ui-pdp-title']"));
            System.out.println(el3.getText());
            
-           WebElement el4 = driver.findElements(By.xpath("//span[@class='price-tag-fraction']")).get(0);
+           newExplicitWait(driver, "//h1[@class='ui-pdp-title']");
+           
+           WebElement el4 = driver.findElements(By.xpath("//span[@class='price-tag-fraction']")).get(1);
            System.out.println(el4.getText());
            
            String el3Text = el3.getText();
            String el4Text = el4.getText();
            
            final SoftAssert softAssert = new SoftAssert();
-		   softAssert.assertTrue(el1Text.equalsIgnoreCase(el3Text));
-		   softAssert.assertTrue(el2Text.equalsIgnoreCase(el4Text));
+		   softAssert.assertTrue(el1Text.trim().equalsIgnoreCase(el3Text.trim()));
+		   softAssert.assertTrue(el2Text.trim().equalsIgnoreCase(el4Text.trim()));
 		   softAssert.assertAll();
        }
        catch(Exception e) 
